f411fd3a426bd8e91af76f1cf84d5e74f90adbf6
arunamballa <arunamballa24@gmail.com>
2025-02-18 17:20:11 -0800

    Initial commit - Spring Boot Temporary project

---
+++ src/test/java/com/arun/temporary/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.temporary.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
